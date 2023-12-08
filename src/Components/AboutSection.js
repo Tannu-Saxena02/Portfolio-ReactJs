43ce57482d534e20b1145a188194f6672216b7b6
Tannu Saxena <tannusaxena02@gmail.com>
2023-12-08 16:03:49 +0530

    UI updates

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -1,66 +1,71 @@
-import React from 'react'
+import React from "react";
 import "./HomeSection.css";
-import ResumePdf from '../assets/Resume/Resume.pdf';
-import {
-    AiOutlineDownload,
-  } from "react-icons/ai";
+import ResumePdf from "../assets/Resume/Resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
 export default function AboutSection() {
   return (
     <div id="AboutSection">
-       <section className="aboutSection">
-          <div className="rowAbout">
-            <div className="aboutPhoto">
-              <img
-                src={require("../assets/developer.jpg")}
-                alt="Paris"
-                className="aboutPhotoStyle"
-              ></img>
-            </div>
-            <div className="aboutInfo my-4" style={{ width: "50%" }}>
-              <div className="aboutText">
-                About
-                <span className="meText"> Me</span>
+      <section className="aboutSection">
+        <div className="aboutText mt-4">
+          About
+          <span
+            style={{
+              color: "#05B4FF",
+            }}
+          >
+            {" "}
+            Me
+          </span>
+        </div>
+        <div className="rowAbout">
+          <div className="aboutPhoto">
+            <img
+              src={require("../assets/profilephoto1.jpg")}
+              alt="Paris"
+              className="aboutPhotoStyle"
+            ></img>
+          </div>
+          <div className="aboutInfo mt-4">
+            <p className="mt-4 aboutDesc">
+              My name is Tannu Saxena, I'm a Mobile/Frontend Developer. Learning
+              stuffs from internet to add magic to my projects. Completed my
+              B.Tech in Computer Science and polishing my Leadership and
+              Management skills.
+            </p>
+            <div className="row my-3">
+              <div className="columns contact-details">
+                <h3
+                  className="my-3 contactDetailsText"
+                  style={{
+                    color: "#FFFFFF",
+                  }}
+                >
+                  Contact{" "}
+                  <span
+                    style={{
+                      color: "#05B4FF",
+                    }}
+                  >
+                    {" "}
+                    Details
+                  </span>
+                </h3>
+                <p
+                  className="address"
+                  style={{
+                    color: "#C7C4C7",
+                  }}
+                >
+                  <span>Tannu Saxena</span>
+                  <br />
+                  <span>Brij Vihar Colony, muradnagar, GZB</span>
+                  <br />
+                  <span>+91 70093-77116</span>
+                  <br />
+                  <span>tannusaxena023@gmail.com</span>
+                </p>
               </div>
-              <p className="my-4 aboutDesc">
-                My name is Shubham, I'm a MERN Stack Developer. Learning stuffs
-                from internet to add magic to my projects. Completed my BE in
-                Computer Science and polishing my Leadership and Management
-                skills.
-              </p>
-              <div className="row my-3">
-                <div className="columns contact-details">
-                  <h3
-                    className="my-3 contactDetailsText"
-                    style={{
-                      color: "#FFFFFF",
-                    }}
-                  >
-                    Contact{" "}
-                    <span
-                      style={{
-                        color: "#05B4FF",
-                      }}
-                    >
-                      {" "}
-                      Details
-                    </span>
-                  </h3>
-                  <p
-                    className="address"
-                    style={{
-                      color: "#C7C4C7",
-                    }}
-                  >
-                    <span>Tannu Saxena</span>
-                    <br />
-                    <span>Brij Vihar Colony, muradnagar, GZB</span>
-                    <br />
-                    <span>+91 70093-77116</span>
-                    <br />
-                    <span>tannusaxena023@gmail.com</span>
-                  </p>
-                </div>
-                <div className="columns download my-4">
+              <div className="columns download my-4">
                 <a
                   href={ResumePdf}
                   download="Resume-PDF-document"
@@ -71,17 +76,15 @@ export default function AboutSection() {
                     type="button"
                     className="btn btn-primary btn-lg downloadButton"
                   >
-                   
-                      <AiOutlineDownload className="downloadIcon" />
-                      Download Resume
-                  
+                    <AiOutlineDownload className="downloadIcon" />
+                    Download Resume
                   </button>
-                  </a>
-                </div>
+                </a>
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from "react";
 import "./HomeSection.css";
-import {
-    AiOutlineArrowUp,
-  } from "react-icons/ai";
+import { AiOutlineArrowUp } from "react-icons/ai";
 export default function Footer() {
   return (
     <div>
-     <div className="footer" id="footer">
-          <div className="designedByText">made by Tannu.</div>
-          <div className="upButton">
-        <a className="nav-link" href="#HomeSection" style={{color:"white",marginInline:40}} >
+      <div className="footer" id="footer">
+        <div className="designedByText">made by Tannu.</div>
+        <div className="upButton">
+          <a
+            className="nav-link"
+            href="#HomeSection"
+            style={{ color: "white" }}
+          >
             <button
               type="button"
               className="btn btn-primary btn-lg arrowBtn"
               // className="arrowBtn"
             >
-              <AiOutlineArrowUp className="arrowBtnIcon" />
+              <AiOutlineArrowUp
+                size={15}
+                style={{
+                  justifyContent: "center",
+                  alignSelf: "center",
+                  alignContent: "center",
+                }}
+                className="arrowBtnIcon mb-1 "
+              />
             </button>
-            </a>
-          </div>
-        </div> 
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
